c4659d971cc1b911fb1e125acd5b4df8f404dc56
Olya <122615170+olyalupatsa@users.noreply.github.com>
2024-01-29 16:34:57 +0200

    1

---
+++ src/main.js
@@ -0,0 +1,133 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import icon from '../src/img/octagon.svg';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const formSearch = document.querySelector('.form');
+const imageList = document.querySelector('.gallery');
+const preload = document.querySelector('.preload');
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+formSearch.addEventListener('submit', handleSearch);
+
+function handleSearch(event) {
+  event.preventDefault();
+  const searchQuery = event.currentTarget.elements.input.value;
+
+  imageList.innerHTML = '';
+
+  if (!searchQuery.trim()) {
+    iziToast.show({
+      title: '❕',
+      theme: 'light',
+      message: `Please, fill in the search field`,
+      messageSize: '20px',
+      messageColor: '#808080',
+      backgroundColor: '#e7fc44',
+      position: 'topLeft',
+      timeout: 3000,
+    });
+    return;
+  }
+
+  preload.classList.remove('is-hidden');
+
+  fetchImages(searchQuery)
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.show({
+          iconUrl: icon,
+          theme: 'dark',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          messageSize: '16px',
+          messageColor: 'white',
+          backgroundColor: '#EF4040',
+          position: 'topRight',
+          timeout: 5000,
+        });
+      }
+      imageList.innerHTML = createMarkup(data.hits);
+      gallery.refresh();
+    })
+    .catch(handleError)
+    .finally(() => preload.classList.add('is-hidden'));
+
+  event.currentTarget.reset();
+}
+
+function fetchImages(value) {
+  const BASE_URL = 'https://pixabay.com/api';
+
+  const searchParams = new URLSearchParams({
+    key: '41989541-8f5a4609d6994378f5ee88908',
+    q: value,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  });
+
+  return fetch(`${BASE_URL}/?${searchParams}`).then(res => {
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
+    return res.json();
+  });
+}
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
+        `<li class="gallery-item">
+        <a class="gallery-link" href="${largeImageURL}">
+           <img
+            class="gallery-image"
+            src="${webformatURL}"
+            alt="${tags}"
+          />
+        </a>
+        <div class="container-additional-info">
+        <div class="container-descr-inner"><p class="description">Likes</p><span class="description-value">${likes}</span></div>
+        
+        <div class="container-descr-inner"><p class="description">Views</p><span class="description-value">${views}</span></div>
+        
+
+        <div class="container-descr-inner"><p class="description">Comments</p><span class="description-value">${comments}</span></div>
+        
+
+        <div class="container-descr-inner"><p class="description">Downloads</p><span class="description-value">${downloads}</span></div>
+        
+        </div>
+      </li>`
+    )
+    .join('');
+}
+
+function handleError(err) {
+  console.error(err);
+  imageList.innerHTML = '';
+  iziToast.show({
+    iconUrl: icon,
+    theme: 'dark',
+    message: 'Sorry, there is a problem with connection with the server.',
+    messageSize: '16px',
+    messageColor: 'white',
+    backgroundColor: '#EF4040',
+    position: 'center',
+    timeout: 5000,
+  });
+}
