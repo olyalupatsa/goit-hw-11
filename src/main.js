4770c384b8496cd582f048d24954703008e4f851
Olya <122615170+olyalupatsa@users.noreply.github.com>
2024-02-01 14:28:26 +0200

    1

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import icon from '../src/img/octagon.png';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
